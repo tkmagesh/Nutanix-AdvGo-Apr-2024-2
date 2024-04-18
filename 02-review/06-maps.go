c156e24586ca29c4fc180bc3f7cd35c5bf608bf5
Magesh Kuppan <tkmagesh77@gmail.com>
2024-04-18 15:10:27 +0530

    maps

--- 02-review/06-maps.go
+++ 02-review/06-maps.go
@@ -15,8 +15,25 @@ func main() {
 
 	fmt.Println("len(productRanks) :", len(productRanks))
 
+	fmt.Println("Adding a new key")
+	productRanks["mouse"] = 2
+	fmt.Println(productRanks)
+
 	fmt.Println("Iterating using range")
 	for key, val := range productRanks {
 		fmt.Printf("productRanks[%q] = %d\n", key, val)
 	}
+
+	fmt.Println("Check for the existence of a key")
+	if rank, exists := productRanks["scribble-pad"]; exists {
+		fmt.Println("Rank of scribble-pad :", rank)
+	} else {
+		fmt.Println("scribble-pad is not ranked yet!")
+	}
+
+	fmt.Println("Removing an item")
+	keyToRemove := "scribble-pad"
+	delete(productRanks, keyToRemove)
+	fmt.Printf("After removing %q, productRanks = %v\n", keyToRemove, productRanks)
+
 }
